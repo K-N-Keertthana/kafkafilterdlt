5210d747601df29dc80bad0f660bf6308355a850
keertthana.kn <keertthana.kn@thoughtworks.com>
2024-11-16 16:58:31 +0530

    Inital commit

---
+++ src/main/java/com/kafka/example/kafkafilterdlt/KafkafilterdltApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.example.kafkafilterdlt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkafilterdltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkafilterdltApplication.class, args);
+	}
+
+}
